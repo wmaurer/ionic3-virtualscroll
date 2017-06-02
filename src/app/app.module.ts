87b91a86645a0f66faafc22ca85a4fc65114cc7f
Wayne Maurer <wmaurer@users.noreply.github.com>
2017-06-02 10:15:01 +0200

    added code for virtualScroll problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,23 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: ErrorHandler, useClass: IonicErrorHandler }
+    ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+interface Item {
+    id: number;
+    value: number;
+}
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    public items = this.createItems();
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
+    }
 
-  }
+    increment(item: Item) {
+        this.items = this.items.map(i => i !== item ? i : { id: item.id, value: item.value + 1 });
+    }
 
+    createItems(): Item[] {
+        const arraySize = 10000;
+        const items = new Array(arraySize);
+        for (var i = 0; i < 10000; i++) {
+            items[i] = { id: i, value: 0 };
+        }
+        return items;
+    }
+
+    trackByFn(index, item: Item) {
+        return item.id;
+    }
 }
